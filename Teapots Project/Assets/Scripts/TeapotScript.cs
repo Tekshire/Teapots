db5eb2961e30a3963bdeb4e17d919015f77b6ee1
Keithen Hayenga <keithen@tekshire.com>
2020-04-13 15:26:20 -0700

    Add script for teapot motion.
    First pass allows for experimentation with geometry calculations to determine path. This did not allow for realistic path changes due to collisions, so we won't use this in the game.

---
+++ Teapots Project/Assets/Scripts/TeapotScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeapotScript : MonoBehaviour
+{
+    public float xSpread = 10;
+    public float zSpread = 10;
+    public float yOffset;
+    //public Transform centerPoint;
+
+    public float rotSpeed = 2;
+    public bool rotateClockwise;
+    public bool doRotate = false;
+
+    float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yOffset = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Realign teapots so they all have different forward vectors.
+
+        // Test calculated rotation to see what happens when we have colllisions
+        if (doRotate)
+        {
+            timer += Time.deltaTime * rotSpeed;
+            Rotate();
+        }
+    }
+
+    // Really liked this rotate script to drive teapots in circle.
+    // Not sure how flexible this would be if collisions force teapots out of circle.
+    void Rotate()
+    {
+        if (rotateClockwise)
+        {
+            float x = -Mathf.Cos(timer) * xSpread;
+            float z = Mathf.Sin(timer) * zSpread;
+            Vector3 pos = new Vector3(x, yOffset, z);
+            //transform.position = pos + centerPoint.position;
+            transform.position = pos;
+        }
+        else
+        {
+            float x = Mathf.Cos(timer) * xSpread;
+            float z = Mathf.Sin(timer) * zSpread;
+            Vector3 pos = new Vector3(x, yOffset, z);
+            //transform.position = pos + centerPoint.position;
+            transform.position = pos;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        doRotate = false;
+    }
+}
