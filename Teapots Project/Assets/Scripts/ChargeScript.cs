4ef3bdeccb7185a2334db2d2d12fbca02a9741d6
Keithen Hayenga <keithen@tekshire.com>
2020-04-29 02:03:52 -0700

    Player and shots added.
    The player which is the Tempest claw is added to the center of the scene and the spacebar will fire charges.

---
+++ Teapots Project/Assets/Scripts/ChargeScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargeScript : MonoBehaviour
+{
+    public const float maxRadius = 25f;
+    public const float maxRadiusSq = maxRadius * maxRadius;
+    public float shotRadiusSq;
+    public float shotSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Use Quadratic Equation to calculate radius of shot in globe.
+        // radius = square root(x^2 + y^2 + z^2).
+        // (Don't have to take square root if we compare to redius squared
+        float x = transform.position.x;
+        float y = transform.position.y;
+        float z = transform.position.z;
+
+        if (x * x + y * y + z * z > maxRadiusSq)
+        {
+            // We've gone to far, so destroy charge.
+            Destroy(gameObject);
+        }
+        else
+        {
+            // Normal location, so keep it moving until it hits a trigger.
+            transform.Translate(Vector3.up * Time.deltaTime * shotSpeed);
+        }
+    }
+}
---
+++ Teapots Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput, verticalInput;
+    public float speed = 1.0f;
+    public GameObject chargePrefab;
+
+    private float xRange = 17f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // launch projectile
+            Instantiate(chargePrefab, transform.position, chargePrefab.transform.rotation);
+        }
+
+        // Player move
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        // Limit player movement
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
+
